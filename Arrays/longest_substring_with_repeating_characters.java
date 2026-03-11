b42adf0a2ecd60aad964d2b44cef72739ba514fa
iamgnaneshwarreddy <iam.gnaneshwarreddy@gmail.com>
2026-03-11 16:59:06 +0530

    Adding LeetCode solution for longest_substring_with_repeating_characters

---
+++ Arrays/longest_substring_with_repeating_characters.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> hset = new HashSet<>();
+        int l = 0;
+        int r = l;
+        int maxL = 0;
+        while(r<s.length()){
+            
+            while(hset.contains(s.charAt(r))){
+
+                hset.remove(s.charAt(l));
+                l++;
+            }
+            hset.add(s.charAt(r));
+            maxL = Math.max(maxL,r-l+1);
+            r++;
+
+        }
+        return maxL;
+    }
+} 
